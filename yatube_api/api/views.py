9fdf8db738bd2f706ed50a74a9885f0dae03b135
Aleksey Petukhov <ikazman@yandex.ru>
2021-07-22 15:47:41 +0300

    get_queryset in FollowViewSet tweaks. Ready for second review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import permissions, viewsets
 from rest_framework import filters
 from rest_framework import mixins
 
-from posts.models import Follow, Group, Post, User
+from posts.models import Group, Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer,
                           GroupSerializer, PostSerializer)
